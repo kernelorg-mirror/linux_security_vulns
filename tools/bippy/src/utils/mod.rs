283a43e67f70430dde72ba0700f06abd6e668a2f
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2026-06-16 11:53:21 +0530

    tools: add run_symbool function
    Doesn't do anything yet, but it sets the stage to call the tool.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/bippy/src/utils/mod.rs
+++ tools/bippy/src/utils/mod.rs
@@ -5,12 +5,14 @@
 pub mod dyad;
 pub mod file;
 pub mod git;
+pub mod symbool;
 pub mod text;
 pub mod version;
 
 pub use dyad::run_dyad;
 pub use file::{read_message_file, read_tags_file, read_uuid};
 pub use git::{get_commit_subject, get_commit_text};
+pub use symbool::run_symbool;
 pub use text::strip_commit_text;
 pub use version::{
     determine_default_status, generate_cpe_ranges, generate_git_ranges, generate_version_ranges,
